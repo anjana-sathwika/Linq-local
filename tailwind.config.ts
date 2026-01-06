613f7c0dda01b31a2cb0bc62e5687b384783085c
Anjana Sathwika Reddy Depa <21311a04h7@sreenidhi.edu.in>
2026-01-06 14:56:51 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        accent: "#00E676", // LINQ accent green
-      },
+     colors: {
+       primary: '#2F5EEA',
+       primaryDark: '#1E3FAE',
+       accent: '#5FA9FF',
+       background: '#F5F1E8',
+       textMain: '#0F172A',
+      }
+     }
+    },
       fontFamily: {
         sans: ["var(--font-sora)", "ui-sans-serif", "system-ui", "-apple-system", "Segoe UI", "Roboto", "Helvetica", "Arial", "sans-serif"],
         heading: ["var(--font-sora)", "ui-sans-serif", "system-ui"],
